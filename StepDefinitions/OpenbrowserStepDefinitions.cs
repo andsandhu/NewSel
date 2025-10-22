f0430fbe47aaa866601e22c80bffeaa67ddb61c3
rdsan <rdsan@REPAIR>
2025-10-22 11:36:37 -0400

    simple prog

--- StepDefinitions/OpenbrowserStepDefinitions.cs
+++ StepDefinitions/OpenbrowserStepDefinitions.cs
@@ -20,13 +20,19 @@ namespace NewSel.StepDefinitions
         [When("enter username and password and click on Submit button")]
         public void WhenEnterUsernameAndPasswordAndClickOnSubmitButton()
         {
-            
+            IWebElement e = dr.FindElement(By.Id("username"));
+            //.SendKeys("student");
+            e.SendKeys("student");
+            IWebElement f = dr.FindElement(By.Id("password"));
+                f.SendKeys("Password123");
+                IWebElement s= dr.FindElement(By.Id("submit"));
+            s.Click();
         }
 
         [Then("logged in successfully page should be displayed")]
         public void ThenLoggedInSuccessfullyPageShouldBeDisplayed()
         {
-           dr.Close();
+          dr.Close();
         }
     }
 }
